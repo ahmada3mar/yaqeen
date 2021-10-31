cb8bbb6b656b746348e923ac82afa0594f512d36
server side <ahmada3mar@gmail.com>
2021-10-31 09:11:01 +0200

    Https redirect

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Http;
 
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
+
 class Kernel extends HttpKernel
 {
     /**
@@ -37,6 +38,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\HttpRedirect::class,
+
         ],
 
         'api' => [
